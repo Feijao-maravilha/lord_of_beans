5b95e15d5cd237be08ab4bf667839183fe8d1be2
Feijao-maravilha <calfer2010@gmail.com>
2026-09-12 13:40:37 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/o-que-e")
+def o_que_e():
+    return render_template("o_que_e.html")
+
+@app.route("/banner")
+def banner():
+    return render_template("banner.html")
+
+@app.route("/videos")
+def videos():
+    return render_template("videos.html")
+
+@app.route("/fotos")
+def fotos():
+    return render_template("fotos.html")
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
