15f146b91d595427e0b92a6d1bf03f87f0835a74
Feijao-maravilha <calfer2010@gmail.com>
2026-09-20 14:14:35 -0300

    Corrige variável de porta para hospedagem

--- app.py
+++ app.py
@@ -27,6 +27,6 @@ def set_language(lang):
         session['lang'] = lang
     return redirect(request.referrer or url_for('home'))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", "5000"))
 app.run(debug=False, host="0.0.0.0", port=port)
