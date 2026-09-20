87f6ea3935d78b10ad19ab4010534981bf2c6ab7
Evierthal <147686960+Evierthal@users.noreply.github.com>
2026-09-20 13:50:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,5 @@ def set_language(lang):
     return redirect(request.referrer or url_for('home'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    port = int(os.environ.get("PORT", "5000"))
+app.run(debug=False, host="0.0.0.0", port=port)
